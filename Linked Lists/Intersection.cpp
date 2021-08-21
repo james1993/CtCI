4347c07c351ed0ba0f22194c7d6c77cff10bc9a5
James Ogletree <jamesogletree@gmail.com>
2021-08-21 13:11:34 -0500

    2.7

---
+++ Linked Lists/Intersection.cpp
@@ -0,0 +1,96 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+void prettyPrintLinkedList(ListNode* node) {
+  while (node && node->next) {
+      cout << node->val << "->";
+      node = node->next;
+  }
+
+  if (node) {
+    cout << node->val << endl;
+  } else {
+    cout << "Empty LinkedList" << endl;
+  }
+}
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+{
+    ListNode *p1 = headA;
+    ListNode *p2 = headB;
+        
+    if (p1 == NULL || p2 == NULL) return NULL;
+
+    while (p1 != NULL && p2 != NULL && p1 != p2) 
+    {
+        p1 = p1->next;
+        p2 = p2->next;
+
+        if (p1 == p2) return p1;
+        if (p1 == NULL) p1 = headB;
+        if (p2 == NULL) p2 = headA;
+    }
+        
+    return p1;
+}
+
+int main() {
+    string line;
+    ListNode* head1 = stringToListNode("[1,2,3,4]");
+    ListNode* head2 = stringToListNode("[2,3,4]");
+    ListNode* intersection = new ListNode(10);
+    head1->next = intersection;
+    head2->next = intersection;
+    
+    
+    return 0;
+}
