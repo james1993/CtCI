cabbd27c18f3c6163429c97527dd365d62780d95
James Ogletree <jamesogletree@gmail.com>
2021-08-23 20:23:09 -0500

    Create Three in One.cpp

---
+++ Stacks and Queues/Three in One.cpp
@@ -0,0 +1,7 @@
+/*
+Describe how you could use a single array to implement three stacks.
+
+1. Divide the array into three equal parts and allow the individual stack to grow in that limited space.
+2. Flexible stacks. When one stack exceeds its initial capacity, we grow the allowable capacity and shift elements as necessary.
+
+*/
